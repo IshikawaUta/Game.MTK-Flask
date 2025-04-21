86ad5fa8dfcc7a557ed6aad0c3cb58050ed10e0b
IshikawaUta <Jayaj7129@gmail.com>
2025-04-21 19:09:02 +0700

    update

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/uta09/blok-puzzle/venv
+set -gx VIRTUAL_ENV /home/uta09/game-mtk/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
